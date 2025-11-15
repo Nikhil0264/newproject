fc2a9544bd92fa4bf336c30f87ddf520955a7e05
Nikhil0264 <nikhilpadul95@gmail.com>
2025-11-15 13:28:11 +0530

    user model correction

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     profileImage:{type:String,default:""},
-    cleriId:{type:String,required:true,unique:true},
+    clerkId:{type:String,required:true,unique:true},
 },{timestamps:true});
 const User = mongoose.model('User', userSchema);
 
