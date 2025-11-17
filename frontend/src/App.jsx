dc771e45dc1769e11a5907f7c530a174b1bb77b2
Nikhil0264 <nikhilpadul95@gmail.com>
2025-11-18 01:28:21 +0530

    making the problem page format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,10 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser }
 import { Navigate, Route,Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 
-import ProblemPage from './pages/ProblemPage';
+import ProblemsPage from './pages/ProblemsPage';
 import { Toaster } from 'react-hot-toast';
 import DashboardPage from './pages/DashboardPage';
+import ProblemPage from './pages/ProblemPage';
 
 function App() {
    const {isSignedIn,isLoaded} = useUser();
@@ -16,7 +17,8 @@ function App() {
       <Routes>
       <Route path="/" element={!isSignedIn ? <HomePage/> : <Navigate to = {"/dashboard"} />} />
       <Route path="/dashboard" element={isSignedIn ? <DashboardPage/> : <Navigate to = {"/"} />} />
-      <Route path="/problem" element={isSignedIn ? <ProblemPage/> : <Navigate to = {"/"} />} />
+      <Route path="/problems" element={isSignedIn ? <ProblemsPage/> : <Navigate to = {"/"} />} />
+      <Route path="/problem/:id" element={isSignedIn ? <ProblemPage/> : <Navigate to = {"/"} />} />
       </Routes>
     <Toaster  toastOptions={{duration:2000}}/>
     </>
