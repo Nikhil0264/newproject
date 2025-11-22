6cede813cfa217276f11788c2dbc67ead36853f6
Nikhil0264 <nikhilpadul95@gmail.com>
2025-11-23 00:58:12 +0530

    video calling is ready

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,18 +1,17 @@
-import { chatClint } from "../lib/stream.js";
+import { chatClient } from "../lib/stream.js";
 
-export async function getStreamToken(req,res) {
-    try{
-        const token = chatClint.createToken(req.user.clerkId);
+export async function getStreamToken(req, res) {
+  try {
+    const token = chatClient.createToken(req.user.clerkId);
 
-        req.res.status(200).json({
-            token,
-            userId:req.user.clerkId,
-            userName:req.user.name,
-            userImage:req.user.profileImage,
-        });
-
-    }catch(error){
-        console.error("Error getting Stream token:",error);
-        res.status(500).json({msg:"Internal server error"});
-    }
-};
+    req.res.status(200).json({
+      token,
+      userId: req.user.clerkId,
+      userName: req.user.name,
+      userImage: req.user.profileImage,
+    });
+  } catch (error) {
+    console.error("Error getting Stream token:", error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+}
--- frontend/src/components/CreateSessionModal.jsx
+++ frontend/src/components/CreateSessionModal.jsx
@@ -19,7 +19,7 @@ function CreateSessionModal({
         <h3 className="font-bold text-2xl mb-6">Create New Session</h3>
 
         <div className="space-y-8">
-          {/* PROBLEM SELECTION */}
+          
           <div className="space-y-2">
             <label className="label">
               <span className="label-text font-semibold">Select Problem</span>
@@ -49,7 +49,7 @@ function CreateSessionModal({
             </select>
           </div>
 
-          {/* ROOM SUMMARY */}
+          
           {roomConfig.problem && (
             <div className="alert alert-success">
               <Code2Icon className="size-5" />
